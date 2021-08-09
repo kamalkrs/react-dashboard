a80c4682eacb6c67866750e7728cf7d64147e6b1
Kamal Kr <kamal.dhn@gmail.com>
2021-08-09 23:00:29 +0530

    first commit

---
+++ src/routes/WebRouter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WebRouter extends Component {
+    render() {
+        return (
+            <div>
+                Web Router
+            </div>
+        )
+    }
+}
+
+export default WebRouter
